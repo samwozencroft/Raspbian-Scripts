824adcbfe859245aa1eb7f8117dcdc86ec46953d
Sam Wozencroft <45769279+samwozencroft@users.noreply.github.com>
2021-11-12 13:03:54 +0000

    Update pi-cleanup.sh

--- pi-cleanup.sh
+++ pi-cleanup.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
- 
- #Installing dependencies
- echo -e $YELLOW"Installing dependencies"$ENDCOLOR
- sudo apt install aptitude -y
- 
+
 OLDCONF=$(dpkg -l|grep "^rc"|awk '{print $2}')
 CURKERNEL=$(uname -r|sed 's/-*[a-z]//g'|sed 's/-386//g')
 LINUXPKG="linux-(image|headers|ubuntu-modules|restricted-modules)"
@@ -12,6 +8,10 @@ OLDKERNELS=$(dpkg -l|awk '{print $2}'|grep -E $LINUXPKG |grep -vE $METALINUXPKG|
 YELLOW="\033[1;33m"
 RED="\033[0;31m"
 ENDCOLOR="\033[0m"
+
+#Installing dependencies
+echo -e $YELLOW"Installing dependencies..."$ENDCOLOR
+sudo apt install aptitude -y
  
 if [ $USER != root ]; then
 echo -e $RED"Error: must be root"
