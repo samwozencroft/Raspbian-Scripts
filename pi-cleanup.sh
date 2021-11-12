3c8c835e43a83c1da76562206576b7d62262852e
Sam Wozencroft <45769279+samwozencroft@users.noreply.github.com>
2021-11-12 13:23:43 +0000

    Update pi-cleanup.sh

--- pi-cleanup.sh
+++ pi-cleanup.sh
@@ -32,6 +32,9 @@ echo -e $YELLOW"Emptying every trashes..."$ENDCOLOR
 rm -rf /home/*/.local/share/Trash/*/** &> /dev/null
 rm -rf /root/.local/share/Trash/*/** &> /dev/null
 
+#Removing unused packages
+apt-get purge --auto-remove scratch debian-reference-en dillo idle3 python3-tk idle python-pygame python-tk lightdm gnome-themes-standard gnome-icon-theme raspberrypi-artwork gvfs-backends gvfs-fuse desktop-base lxpolkit netsurf-gtk zenity xdg-utils mupdf gtk2-engines alsa-utils  lxde lxtask menu-xdg gksu midori xserver-xorg xinit xserver-xorg-video-fbdev libraspberrypi-dev libraspberrypi-doc dbus-x11 libx11-6 libx11-data libx11-xcb1 x11-common x11-utils lxde-icon-theme gconf-service gconf2-common
+
 #Apt Get Clean
 sudo apt-get --yes autoremove
 sudo apt-get --yes autoclean
