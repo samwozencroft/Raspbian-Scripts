f31c1835845e8d45c97f7435879e17af3190c17b
Sam Wozencroft <samwozencroft@hotmail.co.uk>
2022-06-28 10:25:18 +0100

    Create snmp_service_setup_allow_polling.sh

---
+++ SNMP/snmp_service_setup_allow_polling.sh
@@ -0,0 +1,33 @@
+
+!/bin/bash
+
+#######################
+#  snmp_service_setup_allow_polling.sh
+#  by sam wozencroft
+#
+#  version 1.0
+#######################
+
+OLDCONF=$(dpkg -l|grep "^rc"|awk '{print $2}')
+CURKERNEL=$(uname -r|sed 's/-*[a-z]//g'|sed 's/-386//g')
+LINUXPKG="linux-(image|headers|ubuntu-modules|restricted-modules)"
+METALINUXPKG="linux-(image|headers|restricted-modules)-(generic|i386|server|common|rt|xen)"
+OLDKERNELS=$(dpkg -l|awk '{print $2}'|grep -E $LINUXPKG |grep -vE $METALINUXPKG|grep -v $CURKERNEL)
+YELLOW="\033[1;33m"
+RED="\033[0;31m"
+ENDCOLOR="\033[0m"
+
+#Checking For elevated
+if [ $USER != root ]; then
+echo -e $RED"Error: must be root"
+echo -e $YELLOW"Exiting..."$ENDCOLOR
+exit 0
+fi
+
+#Installing dependencies
+echo -e $YELLOW"Installing dependencies..."$ENDCOLOR
+sudo apt update -y && sudo apt upgrade -y
+	#RPI Update
+	sudo apt-get install net-snmp
+  sudo systemctl enable snmpd
+  sudo systemctl start snmpd
